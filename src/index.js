154c9cc9c0526fc8ff728cb906e7deda14da8a35
Raphael R <reis.rrsc@outlook.com>
2025-12-08 14:53:12 -0300

    inicio uso do prompt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import prompt from 'prompt'
+import prompt from "prompt";
+import mainPrompt from "./prompts/prompt-main.js";
 
 async function main() {
-    
+  //   prompt.get(mainPrompt, function (err, result) {}); // Forma 1 de fazer
+  prompt.get(mainPrompt, async (err, choose) => {
+    if (choose.select == 1) console.log("escolheu qr code");
+    if (choose.select == 2) console.log("escolheu password");
+  });
 }
+
+main();
